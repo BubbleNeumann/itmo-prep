b50b30ffd60ccee0a282b196c51e7ab7df1ddc25
dana <bubbleneumann@gmail.com>
2024-07-07 13:07:43 +0400

    greedy strategy for knackback problem

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## itmo-prep
 
+<http://neerc.ifmo.ru/wiki/>
+
 <img src="cat.jpg" alt="drawing" width="400"/>
--- backpack_problem.py
+++ backpack_problem.py
@@ -23,18 +23,52 @@ def get_state_val(state: int) -> int:
     return val
 
 
-# bruteforce solution
+def bruteforce():
+    """
+    Bruteforce soulution. Complexity O(2^n)
+    """
+    max_val = 0
+    max_state = None
 
-max_val = 0
-max_state = None
+    for state in range(state_cnt):
+        weight = get_state_weight(state)
+        val = get_state_val(state)
+        if weight <= max_weight:
+            if val > max_val:
+                max_val = val
+                max_state = state
 
-for state in range(state_cnt):
-    weight = get_state_weight(state)
-    val = get_state_val(state)
-    if weight <= max_weight:
-        if val > max_val:
-            max_val = val
-            max_state = state
+    print(max_val)
+    print(bin(max_state))
 
-print(max_val)
-print(bin(max_state))
+
+def greedy():
+    indices = [i for i in range(len(weights))]
+    res = []
+    res_weight = 0
+    res_val = 0
+
+    while indices:
+        max_val = 0
+        max_index = 0
+        for i in indices:
+            if values[i] > max_val:
+                max_val = values[i]
+                max_index = i
+
+        res_weight += weights[max_index]
+
+        if res_weight > max_weight:
+            break
+
+        res_val += values[max_index]
+        res.append(max_index)
+        indices.remove(max_index)
+
+    print(res_val)
+    print(res)
+
+
+if __name__ == "__main__":
+    bruteforce()
+    greedy()
